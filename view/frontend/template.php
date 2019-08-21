a59960ff2fbbc4abc680610328508f278ffdb949
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-21 12:16:50 +0200

    Ajout des meta...

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -2,6 +2,8 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" lang="fr" content="Solange Harmonie PICARD">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="public/css/style.css">
   </head>
